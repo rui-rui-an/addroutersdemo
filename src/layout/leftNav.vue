dbec011bd1f3676ccb7ffd8bc9ff6d35250f3569
rui-rui-an <1405613785@qq.com>
2022-01-04 23:07:25 +0800

    修改

--- src/layout/leftNav.vue
+++ src/layout/leftNav.vue
@@ -11,8 +11,8 @@
           <i class="el-icon-location"></i>
           <span>导航一</span>
         </template>
-        <el-menu-item-group title="分组1">
-          <el-menu-item index="1-3">选项1</el-menu-item>
+        <el-menu-item-group title="分组2">
+          <el-menu-item index="1-3">选项3</el-menu-item>
         </el-menu-item-group>
         <el-submenu index="1-4">
           <template slot="title">选项4</template>
