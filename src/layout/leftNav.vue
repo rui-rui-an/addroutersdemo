44aea5b53208cb78b8f3bba3106fa4d6425a67ca
rui-rui-an <1405613785@qq.com>
2022-01-08 22:32:29 +0800

    修改

--- src/layout/leftNav.vue
+++ src/layout/leftNav.vue
@@ -13,7 +13,7 @@
         </template>
         <el-menu-item-group>
           <template slot="title">分组一</template>
-          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-1">选项12</el-menu-item>
           <el-menu-item index="1-2">选项2</el-menu-item>
         </el-menu-item-group>
         <el-menu-item-group title="分组2">
