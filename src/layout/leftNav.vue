eb28bf00cca9811dadf0442363e665bf1eb35c81
rui-rui-an <1405613785@qq.com>
2022-01-09 22:50:41 +0800

    xugai

--- src/layout/leftNav.vue
+++ src/layout/leftNav.vue
@@ -16,7 +16,7 @@
           <el-menu-item index="1-1">选项12</el-menu-item>
           <el-menu-item index="1-2">选项2</el-menu-item>
         </el-menu-item-group>
-        <el-menu-item-group title="分组2">
+        <el-menu-item-group title="分组3">
           <el-menu-item index="1-3">选项3</el-menu-item>
         </el-menu-item-group>
         <el-submenu index="1-4">
